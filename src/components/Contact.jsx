e7fdff0135938bdfefe4e5079aacbaee0d1892fa
mmuralidharan <mmuralidharan@opentrends.net>
2023-08-17 22:16:51 +0530

    changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,8 +14,39 @@ const Contact = () => {
 		message: "",
 	});
 	const [loading, setLoading] = useState(false);
-	const handleSubmit = (e) => {};
-	const handleChange = () => {};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setLoading(true);
+		emailjs.send(
+			"service_ck07qko",
+			"template_y7p5kqo",
+			{
+				from_name: form.name,
+				to_name: "Vishnu",
+				from_email: form.email,
+				to_email: "iamvishnuks.65@gmail.com",
+				message: form.message,
+			},
+			"2l6LtLAIeWVTKU26L"
+		)
+		.then(()=>{
+			setLoading(false);
+			alert('Thank you. I will get back to you as soon as possible');
+			setForm({
+				name: '',
+				email:'',
+				message:'',
+			})
+		},(error)=>{
+			setLoading(false);
+			console.log(error);
+			alert('Something went wrong');
+		})
+	};
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setForm({ ...form, [name]: value });
+	};
 	return (
 		<div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
 			<motion.div
@@ -49,11 +80,11 @@ const Contact = () => {
 							onChange={
 								handleChange
 							}
-              placeholder="What's your name?"
+							placeholder="What's your name?"
 							className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
 						/>
 					</label>
-          <label className="flex flex-col">
+					<label className="flex flex-col">
 						<span className="text-white font-medium mb-4">
 							Your Email
 						</span>
@@ -66,16 +97,17 @@ const Contact = () => {
 							onChange={
 								handleChange
 							}
-              placeholder="What's your email?"
+							placeholder="What's your email?"
 							className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
 						/>
 					</label>
-          <label className="flex flex-col">
+					<label className="flex flex-col">
 						<span className="text-white font-medium mb-4">
-							Your Message
+							Your
+							Message
 						</span>
 						<textarea
-              rows="7"
+							rows="7"
 							name="message"
 							value={
 								form.message
@@ -83,24 +115,29 @@ const Contact = () => {
 							onChange={
 								handleChange
 							}
-              placeholder="What do you want to say?"
+							placeholder="What do you want to say?"
 							className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium"
 						/>
 					</label>
-          <button type="submit" className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl">
-            {loading? 'Sending...':'send'}
-          </button>
+					<button
+						type="submit"
+						className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl">
+						{loading
+							? "Sending..."
+							: "send"}
+					</button>
 				</form>
 			</motion.div>
-      <motion.div variants={slideIn(
+			<motion.div
+				variants={slideIn(
 					"right",
 					"tween",
 					0.2,
 					1
 				)}
-        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
-          <EarthCanvas/>
-      </motion.div>
+				className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
+				<EarthCanvas />
+			</motion.div>
 		</div>
 	);
 };
